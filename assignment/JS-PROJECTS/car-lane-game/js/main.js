86e2919d221e20f3f2968ae0d311ddbee315e239
roshan shrestha <roshan.shrestha4u@gmail.com>
2019-12-01 14:55:21 +0545

    background image added and update the enemy random spanness in the game

--- assignment/JS-PROJECTS/car-lane-game/js/main.js
+++ assignment/JS-PROJECTS/car-lane-game/js/main.js
@@ -25,7 +25,6 @@ this.currentLane = 1;
    playerCar.style.background = "yellow";
    this.element = playerCar;
    this.element.style.position = "absolute";
-
  }
 
  this.drawPlayerCar = function(){
@@ -74,9 +73,8 @@ function Lane(screenWidth){
 }
 /**
  * This class of Opponent Car comming through the opposite direction.
- * @param       {element} parentElement this is the parent element
+ * @param       {DOM element} parentElement this is the parent element
  * where all the object are instanciate
- * @constructor
  */
 function Enemy(parentElement){
   this.x=50;
@@ -86,72 +84,102 @@ function Enemy(parentElement){
   this.height = 100;
   this.parentElement = parentElement;
   this.element = null;
-  this.currentLane = Math.floor(Math.random()*3);
-
-   this.initCar = function(){
-     var enemyCar = document.createElement('div');
-     this.parentElement.appendChild(enemyCar);
-     enemyCar.style.width= this.width+ 'px';
-     enemyCar.style.height = this.height+ 'px';
-     enemyCar.style.background = "red";
-     this.element = enemyCar;
-     this.element.style.position = "absolute";
-
-   }
-
-   this.drawEnemyCar = function(){
-     this.element.style.left = this.x-this.carMidPoint+"px";
-     this.element.style.top = this.y+"px";
-   }
-
-   this.setEnemyCarPosition = function(x,y){
-     this.x = x;
-     this.y = y;
-   }
 
 
 
-  }
+ this.initEnemyCar = function(){
+   this.currentLane = Math.floor(Math.random()*3);
+   var enemyCar = document.createElement('div');
+   this.parentElement.appendChild(enemyCar);
+   enemyCar.style.width= this.width+ 'px';
+   enemyCar.style.height = this.height+ 'px';
+   enemyCar.style.background = "red";
+   this.element = enemyCar;
+   this.element.style.position = "absolute";
 
 
+ }
+ this.drawEnemyCar = function(){
+   this.element.style.left = this.x-this.carMidPoint+"px";
+   this.element.style.top = this.y+"px";
+ }
+
+ /**
+  * [description]
+  * @param  {integer} x  X position of the enemy car
+  * @param  {integer} y Y Position of the enemy car
+
+  */
+ this.setEnemyCarPosition = function(x,y){
+   this.x = x;
+   this.y = y;
+ }
+}
 
 /**
  * This is the main Game class which handle all the game loop and
  * game init fucntion of the game
  * @param       {dom element} gameScreen his is the parent element
  *  where all the object are instanciate
-
  */
 function Game(gameScreen){
-  this.GAME_HEIGHT = 500;
+  var that = this;
+  this.enemyArr=[];
+  this.GAME_HEIGHT = 600;
   this.GAME_WIDTH = 400;
-
-var playerCarObj = new Player(gameScreen);
-var enemyCarObj = new Enemy(gameScreen);
-var laneObj = new Lane(this.GAME_WIDTH);
-
+  this.enemyInitCounter=0;
+  this.enemyInitTime = Math.floor((Math.random()*40)+20);
+  var playerCarObj = new Player(gameScreen);
+  var enemyCarObj;
+  this.gameScreen= gameScreen;
+  this.roadMoveSpeed =5;
+  var laneObj = new Lane(this.GAME_WIDTH);
   this.PlayerCarYPosition =(this.GAME_HEIGHT-(playerCarObj.height+10));
-
-
 
   this.createGameScreen = function(){
   //  console.log(laneObj);
     laneObj.createRunningPath();
-    gameScreen.style.height= this.GAME_HEIGHT+"px";
-    gameScreen.style.width= this.GAME_WIDTH+"px";
-    gameScreen.style.background= "green";
-    gameScreen.style.position="relative";
+    this.gameScreen.style.height= this.GAME_HEIGHT+"px";
+    this.gameScreen.style.width= this.GAME_WIDTH+"px";
+    //this.gameScreen.style.background= "green";
+    this.gameScreen.style.position="relative";
+    this.gameScreen.style.background="url('images/road.png')";
+    this.gameScreen.style.backgroundSize ="cover";
+    this.gameScreen.style.backgroundRepeat ="no-repeate";
     playerCarObj.initCar();
     playerCarObj.setPlayerCarPosition(laneObj.carRunningPath[playerCarObj.currentLane],this.PlayerCarYPosition);
     playerCarObj.drawPlayerCar();
-    enemyCarObj.initCar();
-    enemyCarObj.drawEnemyCar();
+    playerCarObj.drawPlayerCar();
+    // enemyCarObj.initEnemyCar();
+    // // console.log(enemyCarObj);
+    // this.enemyArr.push(enemyCarObj);
+    // console.log(this.enemyArr);
 
-
+    // enemyCarObj.setEnemyCarPosition(laneObj.carRunningPath[enemyCarObj.currentLane],enemyCarObj.y);
+    // enemyCarObj.drawEnemyCar();
   }
   this.init = function(){
     this.createGameScreen();
+
   }
+
+  this.initEnemyCarArr = function(){
+    enemyCarObj = new Enemy(gameScreen);
+    enemyCarObj.initEnemyCar();
+    enemyCarObj.setEnemyCarPosition(laneObj.carRunningPath[2],0);
+    this.enemyArr.push(enemyCarObj);
+    console.log(this.enemyArr);
+  }
+
+  this.updateEnemyCar = function(){
+    for (var i = 0; i < this.enemyArr.length; i++) {
+      this.enemyArr[i].setEnemyCarPosition(laneObj.carRunningPath[this.enemyArr[i].currentLane],this.enemyArr[i].y);
+      // this.enemyArr[i].setEnemyCarPosition(laneObj.carRunningPath[this.enemyArr[i].currentLane],this.enemyArr[i].y);
+      this.enemyArr[i].y+=5;
+      this.enemyArr[i].drawEnemyCar();
+    }
+  }
+  //event listener for the key press
   window.addEventListener('keydown',function(event){
     if(event.keyCode == 37){
       if(playerCarObj.currentLane!=0){
@@ -160,10 +188,8 @@ var laneObj = new Lane(this.GAME_WIDTH);
         playerCarObj.drawPlayerCar();
         playerCarObj.currentLane-=1;
       }
-
     }
     if(event.keyCode == 39){
-
       if(playerCarObj.currentLane<2){
       console.log("key pressed right");
         playerCarObj.setPlayerCarPosition(laneObj.carRunningPath[playerCarObj.currentLane+1],this.PlayerCarYPosition);
@@ -171,16 +197,31 @@ var laneObj = new Lane(this.GAME_WIDTH);
           playerCarObj.currentLane+=1;
       }
     }
-
   });
-  //MAIN LOOP OF GAME
+
+  /**
+   * GAME LOOP
+   */
   setInterval(function(){
-    enemyCarObj.setEnemyCarPosition(laneObj.carRunningPath[enemyCarObj.currentLane],this.enemyY);
-    this.enemyY++;
+    //road animation here
+    that.gameScreen.style.backgroundPosition = 0+"px "+ that.roadMoveSpeed+"px";
+    that.roadMoveSpeed+=5;
 
-enemyCarObj.drawEnemyCar();
 
-  },1000);
+    /*
+      enemy spaning and animation here
+     */
+    if(that.enemyInitCounter==that.enemyInitTime){
+      that.initEnemyCarArr();
+      that.enemyInitTime= Math.floor((Math.random()*40)+20);
+      that.enemyInitCounter=0;
+    }
+    if(that.enemyArr.length>0){
+      that.updateEnemyCar();
+    }
+    that.enemyInitCounter++;
+
+  },60);
 }
 
 var gameScreen = document.getElementById('gameScreen');
