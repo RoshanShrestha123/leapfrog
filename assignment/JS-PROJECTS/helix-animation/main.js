0e23c50894f005a6e0147a7004272894c1b30af2
roshan shrestha <roshan.shrestha4u@gmail.com>
2019-12-05 08:32:25 +0545

    assignment complete

--- assignment/JS-PROJECTS/helix-animation/main.js
+++ assignment/JS-PROJECTS/helix-animation/main.js
@@ -6,26 +6,60 @@ function Controller(canvas){
   this.y= 100;
   this.angle =0;
   this.circleArr = [];
+  this.totalRow=10;
   var c = canvas.getContext('2d');
   var circleObj;
 
  this.init = function(){
    for (var i = 0; i < 15; i++) {
-     circleObj = new Circle(c,this.x,this.angle);
+     circleObj = new Circle(c,this.x,this.y,this.angle,i);
      this.circleArr.push(circleObj);
      this.x+=20;
      this.angle +=13;
    }
+
+
+ }
+ this.initCollection = function(){
+   // this.init();
+   //upper section
+this.y=100;
+   for (var i = 0; i < this.totalRow; i++) {
+
+     this.init();
+     this.y+=15;
+     this.angle=0;
+     this.x=100;
+
+   }
+   //lower section
+
+ this.y=100;
+   for (var j = 0; j < this.totalRow; j++) {
+     this.init();
+     this.y+=15;
+
+     this.angle=180;
+     this.x=100;
+
+
+   }
+
+ }
+
+ this.singleRowAnimation = function(y){
+   for (var i = 0; i < this.circleArr.length; i++) {
+     this.circleArr[i].update();
+
+
+   }
  }
   setInterval(function(){
     c.clearRect(0,0,canvas.width,canvas.height);
-    for (var i = 0; i < that.circleArr.length; i++) {
-      that.circleArr[i].update();
-
-    }
-  },10);
+    that.singleRowAnimation();
+  },14);
 
 }
 var canvas = document.getElementById('canvas');
 var controllerObj = new Controller(canvas);
-controllerObj.init();
+controllerObj.initCollection();
--- assignment/JS-PROJECTS/helix-animation/sine.js
+++ assignment/JS-PROJECTS/helix-animation/sine.js
@@ -1,22 +1,49 @@
-function Circle(c,x,angle){
+function Circle(c,x,y,angle,size){
   var that = this;
   this.centerX = x;
-  this.centerY = 100;
-  this.radius = 10;
+  this.centerY = y;
+  this.temp=this.centerY;
   this.angle =angle;
+  this.radius = 0;
+  this.counter =0;
+  this.y=y;
   this.draw = function(){
     c.beginPath();
+    var my_gradient = c.createLinearGradient(300, 0, 300, 300);
+    my_gradient.addColorStop(0.3 , "#fcac76");
+    my_gradient.addColorStop(0.5 ,"#f8948d");
+    my_gradient.addColorStop(1, "#e580a2");
+    c.fillStyle = my_gradient;
     c.arc(this.centerX,this.centerY,this.radius, 0 , 2 * Math.PI);
     c.fill();
   }
 
   this.update = function(){
-    this.centerY=100+ 50* Math.cos(this.angle*Math.PI/180);
+    this.centerY=y+ 40* Math.cos(this.angle*Math.PI/180);
     this.angle++;
+    if(Math.cos(this.angle*Math.PI/180)<=0){
+
+      this.radius+=0.1;
+      if(this.radius>10){
+        this.radius=10;
+      }
+    }
+    else{
+      this.radius-=0.1;
+      if(this.radius<1){
+        this.radius=1;
+      }
+    }
+
+
     if(this.angle==360){
       this.angle=0;
+
+
     }
+
     this.draw();
+
   }
 
 
